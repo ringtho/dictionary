0c6874cb280e94c0b2035140d33ddcfa6177fd19
Smith Ringtho <sringtho@gmail.com>
2023-03-14 09:04:41 +0000

    Created the header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react"
+import AppLogo from "./assets/images/logo.svg"
+import MoonIcon from "./assets/images/icon-moon.svg"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>SMITH RINGTHO</h1>
+    <div className="app-container">
+      <header className="app-header">
+        <img src={AppLogo} alt="logo" className="logo" />
+        <div className="app-font-slider">
+          <div className="select-div">
+            <div>
+            <select className="select-div">
+                <option defaultValue value="grapefruit">Sans Serif</option>
+                <option value="lime">Serif</option>
+                <option value="coconut">Mono</option>
+              </select>
+            </div>
+          </div>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+          <img src={MoonIcon} alt="moon-icon" className="moon-icon" />
+        </div>
       </header>
     </div>
   );
