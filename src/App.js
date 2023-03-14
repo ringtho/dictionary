b4b7d5a32b4892c0b031868fd5bf8d04b8e10bca
Smith Ringtho <sringtho@gmail.com>
2023-03-14 11:25:47 +0000

    Added noun section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react"
 import AppLogo from "./assets/images/logo.svg"
 import MoonIcon from "./assets/images/icon-moon.svg"
+import SearchIcon from "./assets/images/icon-search.svg"
+import PlayIcon from "./assets/images/icon-play.svg"
 
 
 function App() {
@@ -10,8 +12,8 @@ function App() {
         <img src={AppLogo} alt="logo" className="logo" />
         <div className="app-font-slider">
           <div className="select-div">
-            <div>
-            <select className="select-div">
+            <div className="select-div">
+            <select>
                 <option defaultValue value="grapefruit">Sans Serif</option>
                 <option value="lime">Serif</option>
                 <option value="coconut">Mono</option>
@@ -25,6 +27,33 @@ function App() {
           <img src={MoonIcon} alt="moon-icon" className="moon-icon" />
         </div>
       </header>
+      <main>
+        <section className="search-bar-container">
+          <input type="text" className="search-bar" />
+          <img src={SearchIcon} alt="search-icon" className="search-icon" />
+        </section>
+        <section>
+          <div className="keyword-container">
+            <div className="word">
+              <h1>Keyboard</h1>
+              <p>/ˈkiːbɔːd/</p>
+            </div>
+            <img src={PlayIcon} alt="play-icon" className="play-icon" />
+          </div>
+          <div className="noun-container">
+            <p className="meaning">Meaning</p>
+            <ul className="list">
+              <li>A set of keys used to operate a typewriter, computer etc.</li>
+              <li>A component of many instruments including the piano, organ, and harpsichord consisting of usually black and white keys that cause different tones to be produced when struck.</li>
+              <li>A device with keys of a musical keyboard, used to control electronic sound-producing devices which may be built into or separate from the keyboard device.</li>
+            </ul>
+            <div>
+              <span className="synonym">Synonyms</span>
+              <span className="synonym-description">electronic keyboard</span>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
