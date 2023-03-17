5434a1bbeabdadecd2a27f7bd7ca7fb77117fdcc
Smith Ringtho <sringtho@gmail.com>
2023-03-17 19:58:55 +0300

    Separated the input search bar into a SearchBar componnent

--- src/components/Main.js
+++ src/components/Main.js
@@ -61,43 +61,41 @@ export default function Main() {
  
     return (
         <>
-       
-        <SearchBar 
-            handleSubmit={handleSubmit} 
-            handleOnChange={handleOnChange} 
-            keyWord={keyWord} 
-            status={status}  
-        />
+            <SearchBar 
+                handleSubmit={handleSubmit} 
+                handleOnChange={handleOnChange} 
+                keyWord={keyWord} 
+                status={status}  
+            />
 
-
-        {error ?  <Error {...error} /> :
-        <section>
-            <div className="keyword-container">
-                <div className="word">
-                    <h1>{wordDetails.word}</h1>
-                    <p>{wordDetails.phonetic}</p>
+            {error ?  <Error {...error} /> :
+            <section>
+                <div className="keyword-container">
+                    <div className="word">
+                        <h1>{wordDetails.word}</h1>
+                        <p>{wordDetails.phonetic}</p>
+                    </div>
+                    <div className="img-container">
+                        <img src={playIcon} alt="play-icon" className="play-icon" onClick={play} />
+                        <img src={playIconHover} alt="play-icon" className="play-icon-hover" onClick={play} />
+                    </div>
+                    
+                    <audio id="audio" src={audioSrc}></audio>
                 </div>
-                <div className="img-container">
-                    <img src={playIcon} alt="play-icon" className="play-icon" onClick={play} />
-                    <img src={playIconHover} alt="play-icon" className="play-icon-hover" onClick={play} />
+                {partOfSpeech}
+                <div className="source-container">
+                    <p className="source">Source</p>
+                    <a id="source-link" className="source-link" 
+                        target="_blank"
+                        rel="noreferrer" 
+                        href={`https://en.wiktionary.org/wiki/${wordDetails.word}`}
+                        >https://en.wiktionary.org/wiki/{wordDetails.word}</a>
+                    <a href={`https://en.wiktionary.org/wiki/${wordDetails.word}`} target="_blank" rel="noreferrer">
+                        <img src={newWindow} alt="" className="new-window" />
+                    </a>
                 </div>
-                
-                <audio id="audio" src={audioSrc}></audio>
-            </div>
-            {partOfSpeech}
-            <div className="source-container">
-                <p className="source">Source</p>
-                <a id="source-link" className="source-link" 
-                    target="_blank"
-                    rel="noreferrer" 
-                    href={`https://en.wiktionary.org/wiki/${wordDetails.word}`}
-                    >https://en.wiktionary.org/wiki/{wordDetails.word}</a>
-                <a href={`https://en.wiktionary.org/wiki/${wordDetails.word}`} target="_blank" rel="noreferrer">
-                    <img src={newWindow} alt="" className="new-window" />
-                </a>
-            </div>
-        </section>
-        }
+            </section>
+            }
         </>
     )
 }
