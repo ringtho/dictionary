a5b4ded15fdc1053bd5a4d4dcc98792a306afa0f
Smith Ringtho <sringtho@gmail.com>
2023-03-22 11:18:36 +0000

    Added Content component for organizing the main content (verbs, nouns) of the app

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,11 +4,6 @@ import Content from "./Content"
 import Error from "./Error"
 import getWord from "../api"
 
-import SpeechType from "./SpeechType"
-import playIcon from "../assets/images/icon-play.svg"
-import playIconHover from "../assets/images/icon-play-hover.svg"
-import newWindow from "../assets/images/icon-new-window.svg"
-
 export default function Main() {
     const [keyWord, setKeyWord] = useState("")
     const [formData, setFormData] = useState({search : "keyboard"})
@@ -42,25 +37,6 @@ export default function Main() {
 
     },[formData.search])
 
-    function play() {
-        let audio = document.getElementById("audio");
-        audio.play();
-    }
-
-
-    const partOfSpeech = wordDetails.meanings?.map((speech, idx) => {
-        return speech.partOfSpeech && <SpeechType key={idx} {...speech} />
-    })
-
-    const audioArr = wordDetails.phonetics?.filter((audio)=>{
-        return audio.audio !== ''
-    }) || null
-
-    const audioSrc = audioArr !== null && audioArr.length > 0 
-    ? audioArr[0].audio : ''
-
-    console.log(wordDetails)
- 
     return (
         <>
             <SearchBar 
@@ -71,33 +47,7 @@ export default function Main() {
             />
 
             { error ?  <Error {...error} /> : 
-
-            <section>
-                <div className="keyword-container">
-                    <div className="word">
-                        <h1>{wordDetails.word}</h1>
-                        <p>{wordDetails.phonetic}</p>
-                    </div>
-                    <div className="img-container">
-                        <img src={playIcon} alt="play-icon" className="play-icon" onClick={play} />
-                        <img src={playIconHover} alt="play-icon" className="play-icon-hover" onClick={play} />
-                    </div>
-                    
-                    <audio id="audio" src={audioSrc}></audio>
-                </div>
-                {partOfSpeech}
-                <div className="source-container">
-                    <p className="source">Source</p>
-                    <a id="source-link" className="source-link" 
-                        target="_blank"
-                        rel="noreferrer" 
-                        href={`https://en.wiktionary.org/wiki/${wordDetails.word}`}
-                        >https://en.wiktionary.org/wiki/{wordDetails.word}</a>
-                    <a href={`https://en.wiktionary.org/wiki/${wordDetails.word}`} target="_blank" rel="noreferrer">
-                        <img src={newWindow} alt="" className="new-window" />
-                    </a>
-                </div>
-            </section>
+            <Content wordDetails={wordDetails} />
             }
         </>
     )
